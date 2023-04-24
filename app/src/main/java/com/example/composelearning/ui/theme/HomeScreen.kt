052ef291d7ee12133b89151088416a63bcaf40c7
Alex Meleshko <meleha.alex@gmail.com>
2023-04-24 15:31:32 +0300

    Add Modifier

--- app/src/main/java/com/example/composelearning/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/example/composelearning/ui/theme/HomeScreen.kt
@@ -1,9 +1,16 @@
 package com.example.composelearning.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -11,7 +18,13 @@ fun HomeScreen() {
     Text(
         text = "Home Screen",
         fontSize = 32.sp,
-        color = Color.Green
+        color = Color.Green,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        modifier = Modifier
+            .background(Color.Yellow)
+            .fillMaxWidth(),
+        textAlign = TextAlign.Center
     )
     //Text("123456789")
 }
