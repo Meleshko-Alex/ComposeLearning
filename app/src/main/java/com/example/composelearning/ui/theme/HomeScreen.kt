064ff2ee3758b8cbdb8d02d13e9fde59ed2c7644
Alex Meleshko <meleha.alex@gmail.com>
2023-04-25 11:33:45 +0300

    Add attributes for the Column

--- app/src/main/java/com/example/composelearning/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/example/composelearning/ui/theme/HomeScreen.kt
@@ -1,10 +1,14 @@
 package com.example.composelearning.ui.theme
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -14,9 +18,20 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
-    Column {
+    Column(
+
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
+        modifier = Modifier
+            .background(Color.Cyan)
+            .fillMaxSize()
+    ) {
         Text(text = "Title", fontSize = 32.sp)
-        Text(text = "Description", fontSize = 20.sp)
+        Text(
+            text = "Description",
+            fontSize = 20.sp,
+            modifier = Modifier.align(Start)
+        )
     }
 }
 
