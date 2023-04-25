b9884b3199a029cd766b55e4b02cea99bca4aa9d
Alex Meleshko <meleha.alex@gmail.com>
2023-04-25 11:18:30 +0300

    Add Column

--- app/src/main/java/com/example/composelearning/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/example/composelearning/ui/theme/HomeScreen.kt
@@ -1,8 +1,8 @@
 package com.example.composelearning.ui.theme
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
@@ -10,11 +10,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
+    Column {
+        Text(text = "Title", fontSize = 32.sp)
+        Text(text = "Description", fontSize = 20.sp)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
+
+@Composable
+fun Text() {
     Text(
         text = "Home Screen",
         fontSize = 32.sp,
@@ -26,11 +39,4 @@ fun HomeScreen() {
             .fillMaxWidth(),
         textAlign = TextAlign.Center
     )
-    //Text("123456789")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomeScreenPreview() {
-    HomeScreen()
 }
