c1062efb26d48d9646d45f11edc2847d52f07b9e
Alex Meleshko <meleha.alex@gmail.com>
2023-04-26 15:11:17 +0300

    Add a shape with gradient and alpha

--- app/src/main/java/com/example/composelearning/screens/BackgroundScreen.kt
+++ app/src/main/java/com/example/composelearning/screens/BackgroundScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,6 +22,24 @@ fun BackgroundScreen() {
                 .width(160.dp)
                 .height(120.dp)
                 .background(color = Color.Cyan, shape = RoundedCornerShape(16.dp))
+        )
+
+        Box(
+            modifier = Modifier
+                .width(160.dp)
+                .height(120.dp)
+                .align(Alignment.BottomCenter)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color.Red,
+                            Color.Yellow,
+                            Color.Green
+                        )
+                    ),
+                    shape = RoundedCornerShape(16.dp),
+                    alpha = .6f
+                )
         )
     }
 }
