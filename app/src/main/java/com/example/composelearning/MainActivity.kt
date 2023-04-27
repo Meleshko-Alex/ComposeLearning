0da678a77dd5886d18b63a5f01162f3227b7112e
Alex Meleshko <meleha.alex@gmail.com>
2023-04-27 13:07:38 +0300

    Add the Checkbox

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val counter = mutableStateOf(0)
+        val checked = mutableStateOf(true)
 
         setContent {
             //HomeScreen()
@@ -21,7 +22,11 @@ class MainActivity : ComponentActivity() {
             //BackgroundScreen()
             //ImageAndIconScreen()
 
-            StateScreen(count = counter, onCounterClick = {counter.value++})
+            StateScreen(
+                count = counter,
+                onCounterClick = { counter.value++ },
+                checked = checked,
+                onCheckedChange = {newCheckedValue ->  checked.value = newCheckedValue})
         }
     }
 }
--- app/src/main/java/com/example/composelearning/screens/StateScreen.kt
+++ app/src/main/java/com/example/composelearning/screens/StateScreen.kt
@@ -1,17 +1,37 @@
 package com.example.composelearning.screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun StateScreen(count: State<Int>, onCounterClick: () -> Unit) {
-    val counterValue = count.value
+fun StateScreen(
+    count: State<Int>,
+    onCounterClick: () -> Unit,
+    checked: State<Boolean>,
+    onCheckedChange: (Boolean) -> Unit
+) {
 
-    Text(
-        text = "Count = $counterValue",
-        modifier = Modifier.clickable(onClick = onCounterClick)
-    )
+    Column {
+        Text(
+            text = "Count = ${count.value}",
+            modifier = Modifier.clickable(onClick = onCounterClick)
+        )
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(checked = checked.value, onCheckedChange = onCheckedChange)
+            Text(
+                text = "Some checkbox text",
+                fontSize = 18.sp,
+                modifier = Modifier.clickable(onClick = {onCheckedChange(!checked.value)})
+            )
+        }
+    }
 }
