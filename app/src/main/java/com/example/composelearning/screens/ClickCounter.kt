d643fe2697468ec97373890c0f7c138b50bfa6de
Alex Meleshko <meleha.alex@gmail.com>
2023-05-02 13:16:49 +0300

    Add EvenOdd class in the Composable fun with fun remember()

--- app/src/main/java/com/example/composelearning/screens/ClickCounter.kt
+++ app/src/main/java/com/example/composelearning/screens/ClickCounter.kt
@@ -1,8 +1,10 @@
 package com.example.composelearning.screens
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.composelearning.data.EvenOdd
 
@@ -11,10 +13,12 @@ fun ClickCounterScreen(
     countValue: Int,
     onCounterClick: () -> Unit
 ) {
-    val evenOdd = EvenOdd()
+    val evenOdd = remember { EvenOdd() }
 
     Text(
         text = "Clicks = $countValue is ${evenOdd.check(countValue)}",
         modifier = Modifier.clickable(onClick = onCounterClick)
     )
+
+    Log.d("AAA", "EvenOdd object hash = ${evenOdd.hashCode()}")
 }
